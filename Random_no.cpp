e7314351cf22e5b77c2b82886133dc842eea9bcc
Kishore K <116585306+Kishorecode2003@users.noreply.github.com>
2024-02-12 20:14:39 +0530

    Number Guessing game

---
+++ Random_no.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Generate a random number between 1 and 100
+    int secretNumber = rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+
+    std::cout << "Welcome to the Guess the Number game!" << std::endl;
+    std::cout << "I have chosen a number between 1 and 100. Can you guess it?" << std::endl;
+
+    do {
+        std::cout << "Enter your guess: ";
+        std::cin >> guess;
+        attempts++;
+
+        if (guess < secretNumber) {
+            std::cout << "Too low! Try again." << std::endl;
+        } else if (guess > secretNumber) {
+            std::cout << "Too high! Try again." << std::endl;
+        } else {
+            std::cout << "Congratulations! You guessed the number in " << attempts << " attempts." << std::endl;
+        }
+    } while (guess != secretNumber);
+
+    return 0;
+}
+
